07037a0a41eda003cc598021b3ce60c9b715e64b
Paul Goldschmidt <paul.goldschmidt@web.de>
2021-04-01 10:43:36 +0200

    Software V1 Done

--- 2_Software/sts/sts.ino
+++ 2_Software/sts/sts.ino
@@ -22,7 +22,6 @@ DHT dht(DHTPIN, DHTTYPE);
 
 bool dhtworking = true;
 void setup() {
-  // put your setup code here, to run once:
   Wire.begin();
   Wire.setClock(400000L);
   oled.begin(&Adafruit128x64, I2C_ADDRESS);
@@ -33,34 +32,34 @@ void setup() {
   oled.println("Sens con: WAIT");
   sensors.begin();
   sensors.requestTemperatures();
-  float temperature = sensors.getTempCByIndex(0); 
+  float temperaturesens = sensors.getTempCByIndex(0); 
   oled.clear();
-  if (temperature == -127) {
+  if (temperaturesens == -127) {
     oled.println("Stating STS (2/3).");
-    oled.println("Sens con: FAILED");
+    oled.println("Sens. con: FAILED");
     while(1);
   }
   else {
     oled.println("Stating STS (2/3).");
-    oled.println("Sens con: SUCCESS");
+    oled.println("Sens. con: SUCCESS");
   }
   oled.clear();
   oled.println("Stating STS (3/3).");
-  oled.println("Sens con: SUCCESS");
+  oled.println("Sens. con: SUCCESS");
   oled.println("DHT con: WAIT");
   dht.begin();
   float h = dht.readHumidity();
   oled.clear();
   if (isnan(h)) {
     oled.println("Stating STS (3/3).");
-    oled.println("Sens con: SUCCESS");
+    oled.println("Sens. con: SUCCESS");
     oled.println("DHT con: FAILED");
     dhtworking = false;
     delay(10000); // Its possible to use the sensor without a working dht sensor
   }
   else {
     oled.println("Stating STS (3/3).");
-    oled.println("Sens con: SUCCESS");
+    oled.println("Sens. con: SUCCESS");
     oled.println("DHT con: SUCCESS");  
   }
   delay(1000);
@@ -69,10 +68,25 @@ void setup() {
   oled.println("");
   oled.println("          Starting STS     ");
   oled.println("      V 1.0 (April 2021)   ");
-  delay(2500);
+  delay(3500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float t = dht.readTemperature(); // Put "true" in the brackets if you want your temperature in fahrenheit.
+  float h = dht.readHumidity();
+  sensors.requestTemperatures();
+  float temperaturesens = sensors.getTempCByIndex(0); 
+  oled.clear();
+  oled.println("STS Measurements");
+  oled.print("Sens. Temp.:   ");
+  oled.print(temperaturesens);
+  oled.println(" C");
+  oled.print("DHT Hum.:      ");
+  oled.print(h);
+  oled.println(" %");
+  oled.print("DHT Temp.:    ");
+  oled.print(t);
+  oled.print(" C");
+  delay(1500);
+  
 }
